57d3d5673afccda523c96719e01c4382c261c287
Karn <146858398+syp-karn@users.noreply.github.com>
2025-08-13 05:29:12 +0000

    Added code optimizations: macros and median of 3 pivot selection

--- Tutorial-1/mysort.c
+++ Tutorial-1/mysort.c
@@ -1,29 +1,48 @@
+// All code optimizations are done with O1 compiler omptimization
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE 100000000  // 1 million elements
+#define SIZE 1000000  // 1 million elements
+#define SWAP(a, b) do { int tmp = (a); (a) = (b); (b) = tmp; } while(0) // Swap macro decreased exec time from 0.26s to 0.14s
 
 // Swap two elements
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+// void swap(int *a, int *b) {
+//     int temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+
+int median_of_three(int a, int b, int c) {   // using median of three for pivot selection didn't provide much speedup; same exec times of around 0.25s
+    if ((a > b) != (a > c)) return a;
+    else if ((b > a) != (b > c)) return b;
+    else return c;
 }
 
 // Partition function for QuickSort
 int partition(int arr[], int low, int high) {
+    int mid = low + (high - low) / 2;
+
+    // find median value
+    int median_val = median_of_three(arr[low], arr[mid], arr[high]);
+
+    // move median to the end as pivot
+    if (median_val == arr[low]) {
+        SWAP(arr[low], arr[high]);
+    } else if (median_val == arr[mid]) {
+        SWAP(arr[mid], arr[high]);
+    }
     int pivot = arr[high];
     int i = (low - 1);
 
     for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
-            swap(&arr[i], &arr[j]);
+            SWAP(arr[i], arr[j]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
+    SWAP(arr[i + 1], arr[high]);
     return (i + 1);
 }
 
